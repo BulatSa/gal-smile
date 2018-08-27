76bb97f639d08818f0117a30f96870a0be7c0c12
Bulat <sadikovba@gmail.com>
2018-08-27 17:57:30 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,30 @@
+﻿<?php include('header.php'); ?>
+
+
+<header class="header-sec">
+	<div class="container">
+		<div class="header">
+			<nav class="header__menu">
+				<ul>
+					<li><a href="#">О нас</a></li>
+					<li><a href="#">Наши работы</a></li>
+					<li><a href="#">Услуги и цены</a></li>
+					<li><a href="#">Отзывы</a></li>
+					<li><a href="#">Команда</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+			</nav>
+			<a href="#" class="header__phone">
+				<i></i>
+				<span>8 (812) 209 01 10</span>
+			</a>
+			<div class="header__contact">
+				<span>Улица Красуцкого 3, корпус 2</span>
+				<p>Режим работы: 9:00 — 21:00</p>
+			</div>
+		</div>
+	</div>
+</header>
+
+
+<?php include('footer.php'); ?>
